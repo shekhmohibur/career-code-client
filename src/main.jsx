2ab153af512b46872ef9a9a65b29133e96cd3f4b
Shekh Mohibur Rahman <mohibur.rahman2003@gmail.com>
2026-01-31 22:08:20 +0600

    solving vercel issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css";
 import { RouterProvider } from "react-router";
 import Router from "./router/Router.jsx";
+import "./index.css";
 import AuthProvider from "./provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
