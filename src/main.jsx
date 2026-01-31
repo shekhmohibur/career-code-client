ff20027a8d2ce6d0885bad54d98c7029198abbc8
Shekh Mohibur Rahman <mohibur.rahman2003@gmail.com>
2026-01-31 23:47:10 +0600

    fixing css issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import Router from "./router/Router.jsx";
-import "./app.css";
+import "./App.css";
 import AuthProvider from "./provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
